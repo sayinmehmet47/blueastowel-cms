f139d7de346cf2842dfa1365d30f6ec677f92ea5
Mehmet Sayin <mehmetsayin@Mehmets-MacBook-Pro.local>
2024-07-19 23:50:03 +0200

    feat: image size remove

--- payload-cms/src/collections/Media.ts
+++ payload-cms/src/collections/Media.ts
@@ -1,34 +1,10 @@
-import { CollectionConfig } from 'payload/types'
+import { CollectionConfig } from 'payload/types';
 
 export const Media: CollectionConfig = {
   slug: 'media',
   upload: {
     staticURL: '/media',
     staticDir: 'media',
-    imageSizes: [
-      {
-        name: 'thumbnail',
-        width: 400,
-        height: 300,
-        position: 'centre',
-      },
-      {
-        name: 'card',
-        width: 768,
-        height: 1024,
-        position: 'centre',
-      },
-      {
-        name: 'tablet',
-        width: 1024,
-        // By specifying `undefined` or leaving a height undefined,
-        // the image will be sized to a certain width,
-        // but it will retain its original aspect ratio
-        // and calculate a height automatically.
-        height: undefined,
-        position: 'centre',
-      },
-    ],
     adminThumbnail: 'thumbnail',
     mimeTypes: ['image/*'],
   },
@@ -45,4 +21,4 @@ export const Media: CollectionConfig = {
     update: () => true,
     delete: () => true,
   },
-}
+};
