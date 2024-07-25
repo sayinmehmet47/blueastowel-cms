287adf3f90b4623fc79e5bdc83137b11b9bff6f4
Mehmet Sayin <mehmetsayin@Mehmets-MacBook-Pro.local>
2024-07-25 20:30:56 +0200

    feat: add role to fist user

--- payload-cms/src/collections/Users.ts
+++ payload-cms/src/collections/Users.ts
@@ -1,4 +1,4 @@
-import { CollectionConfig } from 'payload/types'
+import { CollectionConfig } from 'payload/types';
 
 const Users: CollectionConfig = {
   slug: 'users',
@@ -9,9 +9,42 @@ const Users: CollectionConfig = {
     useAsTitle: 'email',
   },
   fields: [
+    {
+      name: 'roles',
+      type: 'select',
+      options: [
+        {
+          label: 'Admin',
+          value: 'admin',
+        },
+        {
+          label: 'User',
+          value: 'user',
+        },
+      ],
+      defaultValue: 'user',
+      required: true,
+    },
     // Email added by default
     // Add more fields as needed
   ],
-}
+  hooks: {
+    beforeChange: [
+      async ({ data, req, operation }) => {
+        if (operation === 'create') {
+          const users = await req.payload.find({
+            collection: 'users',
+            limit: 1,
+          });
 
-export default Users
+          if (users.totalDocs === 0) {
+            data.roles = 'admin';
+          }
+        }
+        return data;
+      },
+    ],
+  },
+};
+
+export default Users;
