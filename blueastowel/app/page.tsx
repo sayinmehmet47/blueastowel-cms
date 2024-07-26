0e4007be68d4e66c170ef61ba952f6ea7d747c05
Mehmet Sayin <mehmetsayin@Mehmets-MacBook-Pro.local>
2024-07-26 08:41:07 +0200

    feat: use coolify webhook for github actions

--- blueastowel/app/page.tsx
+++ blueastowel/app/page.tsx
@@ -46,7 +46,7 @@ const InfoSection = ({
         <Button variant="secondary">
           <Link href="/explore-collection">
             <div className="flex gap-3 items-center">
-              <span>Explore Collectionss</span>
+              <span>Explore Collectione</span>
               <ArrowRight />
             </div>
           </Link>
