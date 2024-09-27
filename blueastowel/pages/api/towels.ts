a661049d4376072bf0e881616963bbb658e57563
mehmet <sayinmehmet47@gmail.com>
2024-09-27 23:31:12 +0200

    chore: Update dependencies and fix code formatting

--- blueastowel/pages/api/towels.ts
+++ blueastowel/pages/api/towels.ts
@@ -14,6 +14,7 @@ export default async function handler(
       Authorization: `${slug} API-Key ${apiKey}`,
     },
   });
+  // This is the same as `const data = await response.json() as TowelsResponse;`
   const data: TowelsResponse = await response.json();
 
   res.status(200).json(data);
